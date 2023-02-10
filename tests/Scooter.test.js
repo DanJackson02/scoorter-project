8b09b0577c4da16ce74967fb3e8cb10dbf01fa7c
DanJackson02 <dannyjjackson2002@gmail.com>
2023-02-10 17:33:34 +0000

    modified:   package-lock.json 	modified:   src/Scooter.js 	modified:   src/User.js 	modified:   tests/Scooter.test.js 	modified:   tests/User.test.js

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,25 +1,22 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
+const scooter = new Scooter("West end");
+const scooter2 = new Scooter("Coppenhall");
+
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
+describe('scooter integrity checks', () => {
+  test('Instance has correct properties', () => {
+    expect(scooter).toHaveProperty("user", null);
+    expect(scooter).toHaveProperty("charge");
+    expect(scooter).toHaveProperty("serial");
+    expect(typeof scooter.isBroken).toBe("boolean");
+  });
+});
+describe('scooter instance serial incremental', () => {
+  test('serial number increments by 1', () => {
+    const initialSerial = Scooter.serial;
+    expect(Number(Scooter.serial)).toBe(Number(initialSerial+1));
+  });
+});
 
-//Method tests
-describe('scooter methods', () => {
-  // tests here!
-
-  //rent method
-
-  //dock method
-
-  //requestRepair method
-
-  //charge method
-
-})
--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,34 @@
+const { TestWatcher } = require('jest')
 const User = require('../src/User')
 
 // User tests here
-
-// test username
-
-// test password
-
-// test age
-
-// test login
-
-// test logout
+describe("User class integrity checks",() => {
+    const user = new User("Dan","Password1","20")
+    it('Initialises a User',() => {
+        expect(user).toHaveProperty("username");
+        expect(user).toHaveProperty("password");
+        expect(user).toHaveProperty("age");
+    })
+    describe("User should be logged in when requirements are met", () => {
+        it("user logged in when password and username are correct", () => {
+            user.login('Password1', () => {
+                expect(loggedIn).toBe(true);
+            });
+        });
+    });
+    describe("User should not be logged in with incorrect password", () => {
+        it("throws error when password is incorrect", () => {
+            try {
+                user.login('incorrectPassword', () => {});
+            } catch (error) {
+                expect(error.message).toBe('Incorrect Password');
+            }
+        });
+    });
+    describe("User logout", () => {
+        it("sets loggedIn to false when user logs out", () => {
+            user.logout();
+            expect(user.loggedIn).toBe(false);
+        });
+    });
+});
